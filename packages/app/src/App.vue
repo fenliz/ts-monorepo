3ee96f90699cd2dc0046874427cf3d4c6009b856
Forsberg Hedberg Charlie <cfh91@hotmail.com>
2021-09-25 14:47:17 +0200

    feat(app): add dependency to domain-package

--- packages/app/src/App.vue
+++ packages/app/src/App.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+	// This starter template is using Vue 3 <script setup> SFCs
+	// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+	import HelloWorld from './components/HelloWorld.vue'
+
+	const address = { 
+		city: 'Stockholm', 
+		street: 'Gågatan 12', 
+		postalCode: '51234', 
+		country: 'Sweden',
+	};
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <HelloWorld :address="address" />
 </template>
 
 <style>
